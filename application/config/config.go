196c3120b1611170b2d8acb8d0c028743d92239e
falaqzulfa <falaqzulfa@student.telkomuniversity.ac.id>
2024-03-20 21:24:17 +0700

    fix bug get env

--- application/config/config.go
+++ application/config/config.go
@@ -4,14 +4,26 @@ import (
 	"fmt"
 	"github.com/joho/godotenv"
 	"os"
+	"regexp"
 )
 
-func Config(key string, defau string) string {
-	// load .env file
-	err := godotenv.Load(".env")
+const projectDirName = "go-fiber-clean-architecture" // change to relevant project name
+
+func loadEnv() {
+	projectName := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	currentWorkDirectory, _ := os.Getwd()
+	rootPath := projectName.Find([]byte(currentWorkDirectory))
+
+	err := godotenv.Load(string(rootPath) + `/.env`)
+
 	if err != nil {
 		fmt.Println("Error loading .env file")
 	}
+}
+
+func Config(key string, defau string) string {
+	// load .env file
+	loadEnv()
 
 	data := os.Getenv(key)
 
