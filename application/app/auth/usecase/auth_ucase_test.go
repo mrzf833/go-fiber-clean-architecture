53e93e9ce8a033839c4b428cba6b6855d28db48a
falaqzulfa <falaqzulfa@student.telkomuniversity.ac.id>
2024-05-03 05:54:45 +0700

    add authUcase test, and mock

--- application/app/auth/usecase/auth_ucase_test.go
+++ application/app/auth/usecase/auth_ucase_test.go
@@ -1,55 +1,69 @@
 package usecase_test
 
 import (
-	"context"
+	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"go-fiber-clean-architecture/application/app/category/usecase"
+	"go-fiber-clean-architecture/application/app/auth/request"
+	"go-fiber-clean-architecture/application/app/auth/usecase"
 	"go-fiber-clean-architecture/application/domain"
 	"go-fiber-clean-architecture/application/domain/mocks"
 	"testing"
 )
 
-func TestGetByID(t *testing.T) {
+func TestLogin(t *testing.T) {
+	app := fiber.New()
 	// buat mock object
-	mockCategoryRepo := new(mocks.CategoryRepository)
+	mockAuthRepo := new(mocks.AuthRepository)
 	// buat mock data
-	mockCategory := domain.Category{
+	mockAuth := domain.Auth{
 		ID: 1,
-		Name: "Test",
+		Username: "john",
+		Password: "doe",
+	}
+	authCreateRequest := request.AuthCreateRequest{
+		Username: "john",
+		Password: "doe",
 	}
 
-	// testing GetByID success
+	// testing login success
 	t.Run("success", func(t *testing.T) {
-		// kita akan membuat mock object untuk memanggil method GetByID dari CategoryRepository
-		// di on method ini kita akan memanggil method GetByID dari CategoryRepository dan parameternya context(mock.anything), id(mock.AnythingOfType("int64")) yang dibutuhkan dan return mockCategory dan nil
-		mockCategoryRepo.On("GetByID", mock.Anything, mock.AnythingOfType("int64")).Return(mockCategory, nil).Once()
-		// kita membuat object categoryUcase yang menggunakan NewCategoryUseCase dengan parameter mockCategoryRepo
-		categoryUcase := usecase.NewCategoryUseCase(mockCategoryRepo)
-		// kita panggil method GetByID dari categoryUcase
-		res, err := categoryUcase.GetByID(context.TODO(), mockCategory.ID)
-		// kita akan melakukan assertion bahwa errornya nil atau bisa disebut tidak ada error
-		assert.NoError(t, err)
-		// kita akan membandingkan hasil res dengan mockCategory bahwa hasilnya sama
-		assert.Equal(t, mockCategory, res)
-		// kita akan memastikan bahwa mockCategoryRepo sudah dipanggil sesuai dengan ekspektasi
-		mockCategoryRepo.AssertExpectations(t)
+		// kita akan membuat mock object untuk memanggil method GetByID dari AuthRepository
+		// di on method ini kita akan memanggil method GetByUsername dari AuthRepository dan parameternya context(mock.anything), username(mock.AnythingOfType("string")) yang dibutuhkan dan return mockAuth dan nil
+		mockAuthRepo.On("GetByUsername", mock.Anything, mock.AnythingOfType("string")).Return(mockAuth, nil).Once()
+		app.Use(func(c *fiber.Ctx) error {
+			// kita membuat object authUcase yang menggunakan NewAuthUseCase dengan parameter mockAuthRepo
+			authUcase := usecase.NewAuthUseCase(mockAuthRepo)
+			// kita panggil method GetByID dari authUcase
+			res, err := authUcase.Login(c, authCreateRequest)
+			// kita akan melakukan assertion bahwa errornya nil atau bisa disebut tidak ada error
+			assert.NoError(t, err)
+			// kita akan membandingkan hasil res dengan mockAuth bahwa hasilnya sama
+			assert.Equal(t, mockAuth.Username, res["username"].(string))
+			// kita akan memastikan bahwa mockAuthRepo sudah dipanggil sesuai dengan ekspektasi
+			mockAuthRepo.AssertExpectations(t)
+			return nil
+		})
 	})
 
-	// testing GetByID failed
-	t.Run("error-failed", func(t *testing.T) {
-		// kita akan membuat mock object untuk memanggil method GetByID dari CategoryRepository
-		// di on method ini kita akan memanggil method GetByID dari CategoryRepository dan parameternya context(mock.anything), id(mock.AnythingOfType("int64")) yang dibutuhkan dan return nil dan error
-		mockCategoryRepo.On("GetByID", mock.Anything, mock.AnythingOfType("int64")).Return(domain.Category{}, assert.AnError).Once()
-		// kita membuat object categoryUcase yang menggunakan NewCategoryUseCase dengan parameter mockCategoryRepo
-		categoryUcase := usecase.NewCategoryUseCase(mockCategoryRepo)
-		// kita panggil method GetByID dari categoryUcase
-		res, err := categoryUcase.GetByID(context.TODO(), mockCategory.ID)
-		// kita akan melakukan assertion bahwa errornya tidak nil atau bisa disebut ada error
-		assert.Error(t, err)
-		// kita akan membandingkan hasil res dengan mockCategory bahwa hasilnya kosong
-		assert.Empty(t, res)
-		// kita akan memastikan bahwa mockCategoryRepo sudah dipanggil sesuai dengan ekspektasi
-		mockCategoryRepo.AssertExpectations(t)
+	// testing login failed
+	t.Run("failed", func(t *testing.T) {
+		// kita akan membuat mock object untuk memanggil method GetByID dari AuthRepository
+		// di on method ini kita akan memanggil method GetByID dari AuthRepository dan parameternya context(mock.anything), username(mock.AnythingOfType("string")) yang dibutuhkan dan return nil dan error
+		mockAuthRepo.On("GetByUsername", mock.Anything, mock.AnythingOfType("string")).Return(domain.Auth{}, assert.AnError).Once()
+		app.Use(func(c *fiber.Ctx) error {
+			// kita membuat object authUcase yang menggunakan NewAuthUseCase dengan parameter mockAuthRepo
+			authUcase := usecase.NewAuthUseCase(mockAuthRepo)
+			// kita panggil method GetByID dari authUcase
+			res, err := authUcase.Login(c, authCreateRequest)
+			// kita akan melakukan assertion bahwa errornya tidak nil atau bisa disebut ada error
+			assert.Error(t, err)
+			// kita akan membandingkan hasil res dengan mockAuth bahwa hasilnya kosong
+			assert.Empty(t, res)
+			// kita akan memastikan bahwa mockAuthRepo sudah dipanggil sesuai dengan ekspektasi
+			mockAuthRepo.AssertExpectations(t)
+			return nil
+		})
 	})
 }
+
--- application/domain/mocks/AuthUsecase.go
+++ application/domain/mocks/AuthUsecase.go
@@ -1,1 +1,20 @@
 package mocks
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/mock"
+	"go-fiber-clean-architecture/application/app/auth/request"
+)
+
+type AuthUsecase struct {
+	mock.Mock
+}
+
+func (m *AuthUsecase) Login(c *fiber.Ctx, request request.AuthCreateRequest) (map[string]interface{}, error) {
+	ret := m.Called(c.Context(), request.Username)
+	var r0 map[string]interface{}
+	if ret.Get(0) != nil {
+		r0 = ret.Get(0).(map[string]interface{})
+	}
+	return r0, ret.Error(1)
+}
