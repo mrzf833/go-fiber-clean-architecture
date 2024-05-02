cdba02e405562573909e410e790872fe2502cae4
falaqzulfa <falaqzulfa@student.telkomuniversity.ac.id>
2024-05-02 21:46:13 +0700

    add image validation

--- application/app/file_save/request/file_save_request.go
+++ application/app/file_save/request/file_save_request.go
@@ -3,6 +3,6 @@ package request
 import "mime/multipart"
 
 type FileSaveCreateRequest struct {
-	File *multipart.FileHeader `form:"file" validate:"required,file_custom_validate"`
+	File *multipart.FileHeader `form:"file" validate:"required,image_custom_validate"`
 	Coba string `form:"coba" validate:"required"`
 }
--- application/helper/custom_validation.go
+++ application/helper/custom_validation.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"fmt"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"mime/multipart"
@@ -19,7 +20,7 @@ type CustomValidationInterface interface {
 	// type validatenya *multipart.FileHeader
 	FileCustomValidate()
 
-	// register custom validation
+	// register all custom validation
 	RegisterCustomValidation()
 }
 
@@ -32,13 +33,14 @@ func NewCustomValidation(validate *validator.Validate, ctx *fiber.Ctx) (CustomVa
 
 func (cl CustomValidationStruct) RegisterCustomValidation() {
 	cl.FileCustomValidate()
+	cl.ImageCustomValidate()
 }
 
 // nama validatenya file_custom_validate
 // type validatenya *multipart.FileHeader
 func (cl CustomValidationStruct) FileCustomValidate() {
 	type fileValidation struct {
-		file string `validate:"file"`
+		File string `validate:"file"`
 	}
 	cl.validate.RegisterValidation("file_custom_validate", func (fl validator.FieldLevel) bool {
 		// get type field
@@ -59,10 +61,44 @@ func (cl CustomValidationStruct) FileCustomValidate() {
 		defer os.Remove(pathTmp + fileName)
 
 		// validate apakah ini file
-		err := cl.validate.Struct(fileValidation{file: pathTmp + fileName})
+		err := cl.validate.Struct(fileValidation{File: pathTmp + fileName})
 		if err != nil {
 			return false
 		}
 			return true
+	})
+}
+
+// nama validatenya image_custom_validate
+// type validatenya *multipart.FileHeader
+func (cl CustomValidationStruct) ImageCustomValidate() {
+	type fileValidation struct {
+		File string `validate:"image"`
+	}
+	cl.validate.RegisterValidation("image_custom_validate", func (fl validator.FieldLevel) bool {
+		// get type field
+		typeField, _, _ := fl.ExtractType(fl.Field())
+		// check jika type field bukan file atau bisa disebut multipart.FileHeader
+		if typeField.Type().String() != "multipart.FileHeader" {
+			return false
+		}
+		// get file from request
+		file := fl.Field().Interface().(multipart.FileHeader)
+		unixName := strconv.FormatInt(time.Now().UnixMicro(), 10)
+		// set file name
+		fileName := unixName + file.Filename
+		pathTmp := GetApplicationPath() + "/storage/private/tmp/"
+		// save file to tmp directory
+		SaveFile(cl.ctx, &file, pathTmp, fileName)
+		// delete file after validate
+		defer os.Remove(pathTmp + fileName)
+
+		// validate apakah ini file
+		err := cl.validate.Struct(fileValidation{File: pathTmp + fileName})
+		fmt.Println(err)
+		if err != nil {
+			return false
+		}
+		return true
 	})
 }
